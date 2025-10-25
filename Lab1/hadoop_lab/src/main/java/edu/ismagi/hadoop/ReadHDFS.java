c05f9f4cee5e493d6b2768f094253c2245acf5e9
Taha Anz <tahaanz70@gmail.com>
2025-10-25 23:11:02 +0100

    lab2/ read a file in hdfs

---
+++ Lab1/hadoop_lab/src/main/java/edu/ismagi/hadoop/ReadHDFS.java
@@ -0,0 +1,29 @@
+package edu.ismagi.hadoop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+
+public class ReadHDFS {
+    public static void main(String[] args) throws IOException {
+
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+
+        Path nomcomplet = new Path("/user/root/purchases.txt");
+        FSDataInputStream inStream = fs.open(nomcomplet);
+        InputStreamReader isr = new InputStreamReader(inStream);
+        BufferedReader br = new BufferedReader(isr);
+        String line = null;
+
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        inStream.close();
+        fs.close();
+    }
+}
+
